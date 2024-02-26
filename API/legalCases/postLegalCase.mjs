21cebf4e8766f5ab557fb8e340445bd5e6c4e0cd
hnpz22 <131169788+hnpz22@users.noreply.github.com>
2024-02-26 08:29:55 -0500

    ✨ Adding automatic DateTime

--- API/legalCases/postLegalCase.mjs
+++ API/legalCases/postLegalCase.mjs
@@ -2,19 +2,15 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 
 const ddbDocClient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
-
 export const handler = async (event, context) => {
     try {
-
-        const { case_id, case_title, court, created_at, date_field, relevant_statutes, summary } = JSON.parse(event.body);
-
+        const { case_id, case_title, court, date_field, relevant_statutes, summary } = JSON.parse(event.body);
         if (!case_id || !case_title || !court || !date_field || !relevant_statutes || !summary) {
             return {
                 statusCode: 400,
                 body: JSON.stringify({ message: "case_id, case_title, court, created_at, date_field, relevant_statutes, and summary are required." }),
             };
         }        
-
         const newLegalCase = {
             case_id, case_title, court, created_at: new Date().toLocaleDateString(), date_field, relevant_statutes, summary
         };
@@ -22,7 +18,6 @@ export const handler = async (event, context) => {
             TableName: "legalCases",
             Item: newLegalCase,
         }));
-
         return {
             statusCode: 201,
             body: JSON.stringify({ message: "Legal Case Successfully created" }),
